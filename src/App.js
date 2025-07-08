44e983b6fca69217f3fc8ea33424b5e1030ceab9
hyeonjoo <z1108102059@gmail.com>
2025-07-08 17:40:47 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import Movie from "./routes/Movie";
@@ -6,16 +6,14 @@ import Tv from "./routes/Tv";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Tv />} />
-          <Route path="tv" element={<Tv />} />
-          <Route path="movie" element={<Movie />} />
-          <Route path=":type/:id" element={<Detail />}></Route>
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<Tv />} />
+        <Route path="tv" element={<Tv />} />
+        <Route path="movie" element={<Movie />} />
+        <Route path=":type/:id" element={<Detail />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
